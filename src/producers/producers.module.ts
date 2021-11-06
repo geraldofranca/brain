ebb2bd78f099b064d70bebabf338a565548d67cb
Geraldo Franca <geraldofranca@192.168.1.2>
2021-11-06 10:41:11 -0300

    feat: added producer controller

--- src/producers/producers.module.ts
+++ src/producers/producers.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProducerRepository } from './producers.repository';
 import { ProducersService } from './producers.service';
+import { ProducersController } from './producers.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProducerRepository])],
-  providers: [ProducersService]
+  providers: [ProducersService],
+  controllers: [ProducersController]
 })
 export class ProducersModule {}
