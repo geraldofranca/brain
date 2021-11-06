090451840d68d166c956419663904ad4be3ec1cd
Geraldo Franca <geraldofranca@192.168.1.2>
2021-11-06 10:39:57 -0300

    feat: added create producer dto

---
+++ src/producers/create-producer.dto.ts
@@ -0,0 +1,10 @@
+export class CreateProducerDto {
+  cpf_cnpj: string;
+  producer_name: string;
+  farm_name: string;
+  city: string;
+  state: string;
+  total_area_farm: number;
+  planting_area: number;
+  vegetation_area: number;
+}
