4dd48e6071423da4aaea075ced2e027ede3d5b7c
Geraldo Franca <geraldofranca@192.168.1.2>
2021-11-06 10:37:14 -0300

    feat: added producer entity

---
+++ src/producers/producer.entity.ts
@@ -0,0 +1,39 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
+
+@Entity()
+@Unique(['cpf_cnpj'])
+
+export class Producer extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 14 })
+  cpf_cnpj: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 100 })
+  producer_name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 100 })
+  farm_name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 80 })
+  city: string;
+  
+  @Column({ nullable: false, type: 'varchar', length: 2 })
+  state: string;
+
+  @Column({ nullable: false, type: 'decimal', precision: 12, scale: 2 })
+  total_area_farm: number;
+
+  @Column({ nullable: false, type: 'decimal', precision: 12, scale: 2 })
+  planting_area: number;
+
+  @Column({ nullable: false, type: 'decimal', precision: 12, scale: 2 })
+  vegetation_area: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
