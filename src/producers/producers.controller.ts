a5d57bca5c20ab71ac16876b852aa444419e72b0
Geraldo Franca <geraldofranca@192.168.1.2>
2021-11-06 10:55:34 -0300

    feat: added producers controller

--- src/producers/producers.controller.ts
+++ src/producers/producers.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProducerDto } from './create-producer.dto';
+import { ProducersService } from './producers.service';
 
 @Controller('producers')
-export class ProducersController {}
+export class ProducersController {
+  constructor(private producersService: ProducersService) {}
+
+  @Post()
+  async createProducer(@Body() createProducerDto: CreateProducerDto): Promise<CreateProducerDto> {
+    return await this.producersService.createProducer(createProducerDto);
+  }
+}
