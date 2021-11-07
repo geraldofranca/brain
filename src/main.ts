64c371171321ff150af80b230cabb2ece8b8fd13
Geraldo Franca <geraldofranca@192.168.1.2>
2021-11-07 19:14:10 -0300

    chore: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste Brain')
+    .setDescription('API para o teste da Brain')
+    .setVersion('1.0')
+    .addTag('brain')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/producers/producers.controller.ts
+++ src/producers/producers.controller.ts
@@ -5,30 +5,44 @@ import { ProducerDto } from './dto/producer.dto';
 import { ValidationParamsPipe } from '../shared/pipes/validation-params.pipe';
 import { ProducersService } from './producers.service';
 import { FindProducersQueryDto } from './dto/find-producers-query.dto';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('producers')
 @Controller('producers')
 export class ProducersController {
   constructor(private producersService: ProducersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Cria o registro do produtor' })
+  @ApiResponse({ status: 201, description: 'Retorna o status 201 e o objeto criado' })
   async createProducer(@Body(ValidationPipe) producerDto: ProducerDto): Promise<HttpResponse> {
     const producer = await this.producersService.createProducer(producerDto);
     return created(producer);  
   }
 
   @Put()
+  @ApiOperation({ summary: 'Altera o registro do produtor' })
+  @ApiResponse({ status: 200, description: 'Retorna o status 200 e o objeto alterado' })
   async updateProducer(@Body(ValidationPipe) producerDto: ProducerDto): Promise<HttpResponse> {
     const producer = await this.producersService.updateProducer(producerDto);
     return ok(producer);  
   }
 
   @Delete('/:id')
+  @ApiOperation({ summary: 'Remove o registro do produtor' })
+  @ApiResponse({ status: 204, description: 'Retorna o status 204' })
   async removeProducer(@Param('id', ValidationParamsPipe) id: string): Promise<HttpResponse> {
     await this.producersService.removeProducer(id);
     return noContent();
   }
 
   @Get()
+  @ApiOperation({ summary: 'Busca os registros por filtro' })
+  @ApiResponse({ status: 200, description: 'Retorna a lista dos registros encontrados com base no filtro' })
   async findUsers(@Query() query: FindProducersQueryDto) {
     const producers = await this.producersService.findProducers(query);
     return ok(producers);  
